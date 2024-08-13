16d56c987231a45308e95e281b0a992e0ab0ef54
Arsen(Kusik) <arsenahaev052@gmail.com>
2024-08-13 15:54:31 +0300

    dependencys+basemodel

---
+++ pizza_delivery_platform/menu/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Pizza(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
